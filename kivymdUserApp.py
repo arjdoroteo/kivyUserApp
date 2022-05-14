f3725c5d690560400bb955745a2cd4288ff042c8
Adrian Robert Doroteo <adrian56th@gmail.com>
2022-05-14 20:34:06 +0800

    simplified data handling https://www.youtube.com/watch?v=G-Rp41BzGxg

--- kivymdUserApp.py
+++ kivymdUserApp.py
@@ -1,21 +1,16 @@
 from kivymd.app import MDApp
 from kivy.core.window import Window
-from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import ScreenManager
 
 
-class myScreen(ScreenManager):
-    name = ObjectProperty(None)
-
-    def btn(self):
-        print(self.name.text)
-        pass
-
-
 class AntiflameApp(MDApp):
+
     def build(self):
         Window.size = [450, 800]
-        return myScreen()
+        return
+
+    def btn(self):
+        print(self.root.ids.name.text)
 
 
 if __name__ == "__main__":
