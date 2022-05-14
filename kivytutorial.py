ff75b466d129f018f907095773c474346fa05c40
Adrian Robert Doroteo <adrian56th@gmail.com>
2022-05-14 16:50:51 +0800

    further testing of layouts

---
+++ kivytutorial.py
@@ -0,0 +1,20 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
+
+class myGrid(FloatLayout):
+    name = ObjectProperty(None)
+    email = ObjectProperty(None)
+
+    def submitbtn(self):
+        print('Name: ', self.name.text, 'Email: ', self.email.text)
+class TestApp(App):
+
+    def build(self):
+        
+        return myGrid()
+
+if __name__ == "__main__":
+    TestApp().run()
