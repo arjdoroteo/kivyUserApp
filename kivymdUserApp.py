65f6cf7083b0bd2956bd4d0c7c472242de927fc3
Adrian Robert Doroteo <adrian56th@gmail.com>
2022-05-14 18:30:35 +0800

    Started the basic layout of Application

--- kivymdUserApp.py
+++ kivymdUserApp.py
@@ -1,10 +1,21 @@
 from kivymd.app import MDApp
-from kivymd.uix import MDLabel
+from kivy.core.window import Window
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager
+
+
+class myScreen(ScreenManager):
+    name = ObjectProperty(None)
+
+    def btn(self):
+        print(self.name.text)
+        pass
 
 
 class AntiflameApp(MDApp):
     def build(self):
-        return
+        Window.size = [450, 800]
+        return myScreen()
 
 
 if __name__ == "__main__":
