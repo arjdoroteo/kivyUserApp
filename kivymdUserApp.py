7d288654ab6dc7f986265c2050e02b3985d98125
Adrian Robert Doroteo <adrian56th@gmail.com>
2022-06-20 14:22:46 +0800

    optimized retrieval of data

--- kivymdUserApp.py
+++ kivymdUserApp.py
@@ -9,15 +9,19 @@ from pymongo import MongoClient
 from kivy.clock import Clock
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton
+import time
 
 
 class AntiflameApp(MDApp):
     dialog = None
+    cluster = MongoClient(
+        'mongodb+srv://test1:123@cluster0.hfj9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    db = cluster['Flame']
+    collection = db['arjdoroteo']
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        Clock.schedule_once(self.decryption)
-        self.event = Clock.schedule_interval(self.pymongo, 5)
+        Clock.schedule_once(self.decryption, 1)
 
     def build(self):
         Window.size = [450, 800]
@@ -44,20 +48,18 @@ class AntiflameApp(MDApp):
         self.event()
         self.dialog.dismiss()
 
-    def pymongo(self, dt):
-        cluster = MongoClient(
-            'mongodb+srv://test1:123@cluster0.hfj9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-        db = cluster['Flame']
-        collection = db['arjdoroteo']
-        results = collection.find().sort('_id', -1).limit(1)
+    def pymongo(self):
+        dataList = []
+        results = self.collection.find().sort('_id', -1).limit(1)
         for result in results:
             temp = str(result['Temperature'])
+            dataList.append(temp)
             co = str(result['CO'])
+            dataList.append(co)
             lpg = str(result['LPG'])
-
-        self.root.ids.temp.text = temp + ' C'
-        self.root.ids.co.text = co + ' PPM'
-        self.root.ids.lpg.text = lpg + ' PPM'
+            dataList.append(lpg)
+            cipherText = result['User Info']
+            dataList.append(cipherText)
 
         temp_limit = 125
         co_limit = 100
@@ -68,14 +70,19 @@ class AntiflameApp(MDApp):
         else:
             pass
 
+        return dataList
+
+    def displayData(self, dt):
+        dataList = self.pymongo()
+        time.sleep(2)
+        self.root.ids.temp.text = dataList[0] + ' C'
+        self.root.ids.co.text = dataList[1] + ' PPM'
+        self.root.ids.lpg.text = dataList[2] + ' PPM'
+
     def decryption(self, dt):
-        cluster = MongoClient(
-            'mongodb+srv://test1:123@cluster0.hfj9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-        db = cluster['Flame']
-        collection = db['arjdoroteo']
-        results = collection.find().sort('_id', -1).limit(1)
-        for result in results:
-            cipherText = result['User Info']
+        dataList = self.pymongo()
+        time.sleep(2)
+        cipherText = dataList[3]
 
         with open('cipher_file', 'rb') as c_file:
             key = c_file.read(32)
@@ -88,6 +95,8 @@ class AntiflameApp(MDApp):
         user_info_list = stringText.split(",")
         self.root.ids.userinfo.text = user_info_list[0]
         print(user_info_list)
+        time.sleep(2)
+        self.event = Clock.schedule_interval(self.displayData, 5)
 
 
 if __name__ == "__main__":
